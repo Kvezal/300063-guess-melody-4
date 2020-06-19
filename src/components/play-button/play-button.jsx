fcaeff3382ace68c2343c3415758098fd05847ba
Kvezal <muckam49@mail.ru>
2020-06-19 17:11:23 +0800

    feat(component): PlayButton component rewrited to stateless (module3-task1)

--- src/components/play-button/play-button.jsx
+++ src/components/play-button/play-button.jsx
@@ -1,27 +1,21 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 
 
 const PLAY_BUTTON_SIZES = [`small`, `big`];
 
-class PlayButton extends PureComponent {
-  constructor(props) {
-    super(props);
-  }
+const PlayButton = (props) => {
+  const {className, size, onButtonClick, children} = props;
 
-  render() {
-    const {className, size, onButtonClick, children} = this.props;
-
-    const sizeClass = PLAY_BUTTON_SIZES.includes(size) ? `play-button--${size}` : ``;
-    return <button
-      type="button"
-      className={`play-button ${sizeClass} ${className ? className : ``}`}
-      onClick={onButtonClick}
-    >
-      {children}
-    </button>;
-  }
-}
+  const sizeClass = PLAY_BUTTON_SIZES.includes(size) ? `play-button--${size}` : ``;
+  return <button
+    type="button"
+    className={`play-button ${sizeClass} ${className ? className : ``}`}
+    onClick={onButtonClick}
+  >
+    {children}
+  </button>;
+};
 
 PlayButton.propTypes = {
   className: PropTypes.string,
